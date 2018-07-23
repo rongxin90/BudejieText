9dfce435f0fc354b7086f528cc3ba4d88d57d381
桂荣信 <467735740@qq.com>
2018-07-23 12:37:02 +0800

    项目环境搭建

---
+++ BudejieText/BudejieText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BudejieText
+//
+//  Created by 桂荣信 on 2018/7/23.
+//  Copyright © 2018年 guirongxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
